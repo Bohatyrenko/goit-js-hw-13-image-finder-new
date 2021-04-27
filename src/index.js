f03b62e2ec4228f599d297e63cbc01661ebd5881
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-04-27 23:43:22 +0300

    add LoadMoreBtn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import hitsTbs from './templates/hits.hbs';
 import '../src/styles.css';
 import PixabayApiService from './js/pixabay-api-service';
+import LoadMoreBtn from './js/load-more-Btn';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+  // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 const pixabayApiService = new PixabayApiService();
-
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
@@ -20,16 +24,21 @@ function onSearch(e) {
   if (pixabayApiService.query === '') {
     return alert('Sorry, you need to enter search text');
   }
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
   pixabayApiService.resetPage();
   pixabayApiService.fetchArticles().then(hits => {
     clearGallery();
     appendHitsMarkup(hits);
+    loadMoreBtn.enable();
   });
 }
 
 async function onLoadMore(e) {
+  loadMoreBtn.disable();
   const result = await pixabayApiService.fetchArticles();
   appendHitsMarkup(result);
+  loadMoreBtn.enable();
 
   window.scrollTo({
     top: e.pageY,
@@ -37,11 +46,6 @@ async function onLoadMore(e) {
     behavior: 'smooth',
   });
 }
-// function windowsScrolling() {}
-
-// function onLoadMore() {
-//   pixabayApiService.fetchArticles().then(appendHitsMarkup);
-// }
 
 function appendHitsMarkup(hits) {
   refs.gallery.insertAdjacentHTML('beforeend', hitsTbs(hits));
